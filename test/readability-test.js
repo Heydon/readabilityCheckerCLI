6731fbb94530556f83b45566511208206ce5c489
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-02-22 10:31:49 +0100

    Setup minimal test harness

---
+++ test/readability-test.js
@@ -0,0 +1,18 @@
+var Lab = require('lab')
+var Code = require('code')
+
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
+var expect = Code.expect
+
+var Readability = require('../lib/readability')
+
+describe('readability', function (){
+
+    it('works', function (done) {
+
+        done()
+    })
+
+})
